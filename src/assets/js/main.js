8641964ad3d15bc41e90290748ae5278f58324bf
vini23hot <vini23hot@gmail.com>
2018-11-22 11:53:40 -0200

    Added option to use with back-end or no in the code

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,8 +1,8 @@
-(function() {
+(function () {
   const result = document.querySelector('#result')
 
   function initEvent() {
-    document.querySelector('#search').addEventListener('submit', function(e) {
+    document.querySelector('#search').addEventListener('submit', function (e) {
       e.preventDefault()
       if (e.target[0].value) {
         result.innerHTML = ''
@@ -12,17 +12,44 @@
     }, false)
   }
 
-  function changeTextButton (button, text) {
+  function changeTextButton(button, text) {
     button.textContent = text
   }
 
-  function search (form) {
+  function search(form) {
     const formData = new FormData(form)
-    fetch(`http://localhost:3000/foods/${formData.get('name')}`)
+
+
+    // WITH BACK-END:
+
+    // fetch(`http://localhost:3000/foods/${formData.get('name')}`)
+    //   .then(resp => resp.json())
+    //   .then(resp => {
+    //     if (!resp.error) {
+    //       resp.forEach(hint => {
+    //         insertCard(hint.food)
+    //       })
+    //     }
+    //     else {
+    //       changeInput(form[0], 'placeholder', 'We didn\'t found any food.')
+    //       resetInput(form[0])
+    //     }
+    //     changeTextButton(form[1], 'SEARCH')
+    //     changeInput(form[0], 'value', '')
+    //   }).catch(() => {
+    //     changeTextButton(form[1], 'SEARCH')
+    //     changeInput(form[0], 'placeholder', 'An error has occurred. Try again later.')
+    //     resetInput(form[0])
+    //   })
+
+
+    // WITHOUT BACK-END (TO DEPLOY IN SURGE.SH):
+
+    fetch(`https://api.edamam.com/api/food-database/parser?app_id=ca747d07&app_key=722fabaee32b8118f7b1cb2e32b137cf&ingr=${formData.get('name')}`)
       .then(resp => resp.json())
       .then(resp => {
-        if (!resp.error) {
-          resp.forEach(hint => {
+        if (resp.hints.length) {
+          resp.hints.forEach(hint => {
             insertCard(hint.food)
           })
         }
@@ -39,21 +66,21 @@
       })
   }
 
-  function resetInput (input) {
+  function resetInput(input) {
     setTimeout(() => {
       changeInput(input, 'placeholder', 'Type a food or a meal...')
     }, 3000)
   }
 
-  function changeInput (input, prop, value) {
+  function changeInput(input, prop, value) {
     input[prop] = value
   }
 
-  function insertCard (food) {
+  function insertCard(food) {
     result.insertAdjacentHTML('beforeend', buildCard(food))
   }
 
-  function buildCard (data) {
+  function buildCard(data) {
     const energy = data.nutrients.ENERC_KCAL ? `<li><b>Energy: </b><span>${data.nutrients.ENERC_KCAL.toFixed(1)}kcal</span></li>` : ''
     const carbs = data.nutrients.CHOCDF ? `<li><b>Carbs: </b><span>${data.nutrients.CHOCDF.toFixed(1)}g</span></li>` : ''
     const protein = data.nutrients.PROCNT ? `<li><b>Protein: </b><span>${data.nutrients.PROCNT.toFixed(1)}g</span></li>` : ''
